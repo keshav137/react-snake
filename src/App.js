58f5ccec52622cc769e84a9a6e48431b877f9eea
Keshav Malhotra <keshav_137@yahoo.co.in>
2023-05-17 20:43:41 -0700

    Added all files for snake game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.css";
+import Snake from "./components/Snake";
+// Design a snake game
 
-function App() {
+const ROWS = 12;
+const COLUMNS = 10;
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Snake rows={ROWS} columns={COLUMNS} />
     </div>
   );
 }
 
-export default App;
---
+++ src/components/Snake.js
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from 'react';
+import Keycode from 'keycode';
+
+const Cell = ({ fill }) => {
+  let cellClass = '';
+  if (fill === '0') {
+    cellClass = 'Cell';
+  } else {
+    cellClass = 'Cell CellFill';
+  }
+  return <div className={cellClass} />;
+};
+
+const initialSnake = [
+  { x: 0, y: 0 },
+  { x: 0, y: 1 }
+];
+
+const initialFoodPosition = { x: 0, y: 5 };
+
+const containsCell = (arr, cellObj) => {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].x === cellObj.x && arr[i].y === cellObj.y) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const cellsMatch = (cell1, cell2) => {
+  return cell1.x === cell2.x && cell1.y === cell2.y;
+};
+
+const getRandomFoodPosition = (m, n) => {
+  let x = Math.floor(Math.random() * (m - 1));
+  let y = Math.floor(Math.random() * (n - 1));
+  return {
+    x: x,
+    y: y
+  };
+};
+
+const Snake = ({ rows, columns }) => {
+  const [arr, setArr] = useState(initialSnake); // first element is tail.. last element is head
+  const [direction, setDirection] = useState('right');
+  const [foodPosition, setFoodPosition] = useState(initialFoodPosition);
+  console.log('dir: ', direction);
+
+  const gameOver = () => {
+    const headPosition = arr[arr.length - 1];
+    // console.log(headPosition);
+    // console.log(direction);
+
+    const x = headPosition.x;
+    const y = headPosition.y;
+    if (x < 0 || x > rows - 1 || y < 0 || y > columns - 1) {
+      return true;
+    }
+    return false;
+  };
+
+  const isGameOver = gameOver();
+
+  const moveSnake = () => {
+    const headPosition = arr[arr.length - 1];
+    if (!isGameOver) {
+      let newHead;
+      if (direction === 'right') {
+        newHead = { x: headPosition.x, y: headPosition.y + 1 };
+      } else if (direction === 'left') {
+        newHead = { x: headPosition.x, y: headPosition.y - 1 };
+      } else if (direction === 'up') {
+        newHead = { x: headPosition.x - 1, y: headPosition.y };
+      } else {
+        newHead = { x: headPosition.x + 1, y: headPosition.y };
+      }
+      arr.push(newHead);
+      if (cellsMatch(foodPosition, headPosition)) {
+        setFoodPosition(getRandomFoodPosition(rows, columns));
+      } else {
+        arr.shift();
+      }
+      setArr([...arr]);
+    }
+  };
+
+  let handleKeyEvents = (event, direction) => {
+    if (
+      Keycode.isEventKey(event, 'up') &&
+      (direction === 'right' || direction === 'left')
+    ) {
+      console.log('direction: ', direction)
+      console.log('switching up');
+      setDirection('up');
+    } else if (
+      Keycode.isEventKey(event, 'down') &&
+      (direction === 'right' || direction === 'left')
+    ) {
+      console.log('direction: ', direction)
+      console.log('switching down');
+      setDirection('down');
+    } else if (
+      Keycode.isEventKey(event, 'right') &&
+      (direction === 'up' || direction === 'down')
+    ) {
+      console.log('direction: ', direction)
+      console.log('switching right');
+      setDirection('right');
+    } else if (
+      Keycode.isEventKey(event, 'left') &&
+      (direction === 'up' || direction === 'down')
+    ) {
+      console.log('direction: ', direction)
+      console.log('switching left');
+      setDirection('left');
+    }
+  };
+
+  useEffect(() => {
+    moveSnake();
+  }, [direction]);
+
+  useEffect(() => {
+    window.addEventListener('keydown', (event) => handleKeyEvents(event, direction));
+  }, []);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      if (!isGameOver) {
+        moveSnake();
+      }
+    }, 300);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [arr]);
+
+  let grid = [];
+
+  for (var i = 0; i < rows; i++) {
+    var entryRow = [];
+    for (var j = 0; j < columns; j++) {
+      if (
+        containsCell(arr, { x: i, y: j }) ||
+        cellsMatch({ x: i, y: j }, foodPosition)
+      ) {
+        entryRow.push(<Cell fill='1' />);
+      } else {
+        entryRow.push(<Cell fill='0' />);
+      }
+    }
+    grid.push(<div className='Row'>{entryRow}</div>);
+  }
+
+  const resetGame = () => {
+    setDirection('right');
+    setFoodPosition(initialFoodPosition);
+    setArr(initialSnake);
+  };
+
+  if (isGameOver) {
+    window.removeEventListener('keydown', handleKeyEvents);
+  }
+
+  return (
+    <div>{isGameOver ? <button onClick={resetGame}> Reset</button> : grid}</div>
+  );
+};
+
+export default Snake;
